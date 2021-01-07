8a6ad5aa9ff9c48d593e707365d807a320e90f10
Anthony Bologna <bologna.anthony@gmail.com>
2021-01-07 12:26:18 -0800

    Built Levels

--- levels.js
+++ levels.js
@@ -1,5 +1,7 @@
+// ctrl + f 'level x' where x is the level seed data
+
 levels_arr = [
-    {	
+    {	// level 1
         platforms: [{
             pos_x: 400,
             pos_y: 300,
@@ -13,7 +15,7 @@ levels_arr = [
             pos_x: 400,
             pos_y: 375,
             width: 20 }
-    }, {
+    }, { // level 2
         platforms: [{
             pos_x: 100,
             pos_y: 300,
@@ -34,7 +36,92 @@ levels_arr = [
             pos_x: 400,
             pos_y: 375,
             width: 20 } 
-    }, { 
+    },  
+    { // level 3
+        platforms: [{
+            pos_x: 400,
+            pos_y: 325,
+            width: 100,
+            height: 10
+        },{
+            pos_x: 325,
+            pos_y: 315,
+            width: 40,
+            height: 10
+        },{
+            pos_x: 300,
+            pos_y: 300,
+            width: 40,
+            height: 10
+        },{
+            pos_x: 275,
+            pos_y: 285,
+            width: 35,
+            height: 10
+        },{
+            pos_x: 255,
+            pos_y: 270,
+            width: 30,
+            height: 10
+        },{
+            pos_x: 475,
+            pos_y: 315,
+            width: 40,
+            height: 10
+        },{
+            pos_x: 500,
+            pos_y: 300,
+            width: 40,
+            height: 10
+        },{
+            pos_x: 525,
+            pos_y: 285,
+            width: 40,
+            height: 10
+        },{
+            pos_x: 545,
+            pos_y: 270,
+            width: 30,
+            height: 10
+        },{
+            pos_x: 350,
+            pos_y: 175,
+            width: 15,
+            height: 60
+        },{
+            pos_x: 450,
+            pos_y: 175,
+            width: 15,
+            height: 60
+        }], coins: [{
+            pos_x: 350,
+            pos_y: 175
+        }, {
+            pos_x: 450,
+            pos_y: 175
+        }], player: {
+            pos_x: 400,
+            pos_y: 375,
+            width: 20
+        }
+    },  
+    { // level 4
+        platforms: [{
+            pos_x: 400,
+            pos_y: 200,
+            width: 100,
+            height: 10,
+            rotation: 5
+        }], coins: [{
+            pos_x: 0,
+            pos_y: 0
+        }], player: {
+            pos_x: 400,
+            pos_y: 375,
+            width: 20
+        }
+    },
+    { // level 5
         platforms: [{
             pos_x: 650,
             pos_y: 350,
@@ -124,7 +211,7 @@ levels_arr = [
             pos_y: 375,
             width: 10
         }
-    }, {
+    }, { // level 6
         platforms: [{
             pos_x: 400,
             pos_y: 350,
@@ -178,6 +265,15 @@ levels_arr = [
             pos_x: 400,
             pos_y: 20,
             width: 10
+        }
+    }, 
+    {
+        platforms: [{
+
+        }], coins: [{
+
+        }], player: {
+
         }
     },
 ];
--- platform.js
+++ platform.js
@@ -1,9 +1,10 @@
 class Platform {
-    constructor(x, y, width, height) {
+    constructor(x, y, width, height, rotation = 0) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        dfg
     }
 
     display() {
